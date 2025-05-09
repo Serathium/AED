93468ec21d19cf1a88f9a0d68eee39e1479c482a
Serathium <samuel.dossantossilva@hotmail.com>
2025-05-09 14:26:40 -0300

    Add exercise solution

---
+++ ProgramacaoDescomplicada/[C]Lista 9 – Ponteiros/8/8.c
@@ -0,0 +1,16 @@
+/*
+Create a program that contains an array of float containing 10 elements.
+Print the address of each position of this array.
+*/
+
+#include <stdio.h>
+
+int main () {
+    float array[10] = { 1.5, 2.23, 3.14, 4.09, 5, 6, 7, 8, 9, 10 };
+
+    // Iterate through the array, printing the addres and value of every element.
+    for ( int i = 0; i < 10; i++ ) {
+        float *address = &array[i];
+        printf( "Address of element %d: %p\tElement value: %.2f\n", i+1, (void *)address, array[i] );
+    }
+}
