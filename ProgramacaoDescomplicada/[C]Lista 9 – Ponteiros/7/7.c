178e949db5ff03c5196401c5e4cda49f8de324b0
Serathium <samuel.dossantossilva@hotmail.com>
2025-05-09 14:11:32 -0300

    Add exercise solution

--- ProgramacaoDescomplicada/[C]Lista 9 – Ponteiros/7/7.c
+++ ProgramacaoDescomplicada/[C]Lista 9 – Ponteiros/7/7.c
@@ -7,12 +7,24 @@ Print the values of A and B in the main function.
 
 #include <stdio.h>
 
+// Forward declaration of functions.
+void Sum( int *a, int *b );
+
 int main () {
     int A = 10, B = 20;
+    int *ptr1 = &A, *ptr2 = &B;
+    printf( "Before sum:\n" );
+    printf( "A: %d\tB: %d\n\n", A, B );
+
+    Sum( ptr1, ptr2 );
+
+    printf( "After sum:\n" );
+    printf( "A: %d\tB: %d\n", A, B );
     
 
 }
 
+// Stores the sum of both variables in variable a.
 void Sum( int *a, int *b ) {
     *a += *b;
 }
